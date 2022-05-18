9619d9758912041cf9ee41874a4b9c5784cb5668
Victor Gorgal <94933775+VictorGorgal@users.noreply.github.com>
2022-05-18 16:39:40 -0300

    Add files via upload

--- MQTT_SQL/local_server.py
+++ MQTT_SQL/local_server.py
@@ -1,7 +1,6 @@
 import paho.mqtt.client as paho
 import sqlite3
 from os.path import exists
-from multiprocessing import Process
 
 
 class SQL:
@@ -117,51 +116,6 @@ class MQTT:
         self.sql.save(payload)
 
 
-class Interface:
-    def __init__(self):
-        self.sql = SQL('data.db')
-
-        p1 = Process(target=start)  # starts MQTT on another process
-        p1.start()
-
-    def get_daily(self):
-        daily = []
-        cursor = self.sql.open()
-        for row in cursor.execute('select * from daily'):
-            daily.append(row)
-        self.sql.close()
-        return daily
-
-    def get_lifetime(self):
-        lifetime = []
-        cursor = self.sql.open()
-        for row in cursor.execute('select * from lifetime'):
-            lifetime.append(row)
-        self.sql.close()
-        return lifetime
-
-    def get_percentage(self):
-        cursor = self.sql.open()
-
-        ok = 0
-        not_ok = 0
-
-        for row in cursor.execute('select * from daily'):
-            date, _, *sensors = row
-            # convert list of strings into list of booleans
-            sensors = [True if x == '1' else False for x in sensors]
-            if all(sensors):
-                ok += 1
-            else:
-                not_ok += 1
-
-        percentage = round(ok * 100 / (ok + not_ok), 2)
-
-        self.sql.close()
-
-        return percentage
-
-
 def start():
     server = MQTT(topic='cadeira/1',
                   username='victor',
@@ -170,17 +124,3 @@ def start():
 
     print('Starting server...')
     server.client.loop_forever()
-
-
-if __name__ == '__main__':
-    interface = Interface()
-    while True:
-        t = input('insert: ')
-        if t == 'd':
-            print(interface.get_daily())
-        elif t == 'l':
-            print(interface.get_lifetime())
-        elif t == 'p':
-            print(interface.get_percentage())
-        else:
-            print('no')
