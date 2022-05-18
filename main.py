650c4f6fbc20e706df95170454db4ac43fd7476d
Victor Gorgal <94933775+VictorGorgal@users.noreply.github.com>
2022-05-18 16:39:18 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,13 +8,47 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
         self.page_widgets.setCurrentWidget(self.home_page)  # starts on home page
+        self.slide_menu.setMaximumWidth(0)
 
         self.btn_menu.clicked.connect(lambda: self.toggleMenu(48))  # slide menu
 
-        self.btn_home.clicked.connect(lambda: self.page_widgets.setCurrentWidget(self.home_page))  # change pages
-        self.btn_graphs.clicked.connect(lambda: self.page_widgets.setCurrentWidget(self.graph_page))
-        self.btn_conect.clicked.connect(lambda: self.page_widgets.setCurrentWidget(self.connection_page))
-        self.btn_config.clicked.connect(lambda: self.page_widgets.setCurrentWidget(self.config_page))
+        self.btn_home.clicked.connect(self.home_btn_func)  # side buttons
+        self.btn_graphs.clicked.connect(self.graphs_btn_func)
+        self.btn_connect.clicked.connect(self.connect_btn_func)
+        self.btn_config.clicked.connect(self.config_btn_func)
+
+        self.highlight_button()
+
+    def home_btn_func(self):
+        self.page_widgets.setCurrentWidget(self.home_page)
+        self.highlight_button()
+
+    def graphs_btn_func(self):
+        self.page_widgets.setCurrentWidget(self.graph_page)
+        self.highlight_button()
+
+    def connect_btn_func(self):
+        self.page_widgets.setCurrentWidget(self.connection_page)
+        self.highlight_button()
+
+    def config_btn_func(self):
+        self.page_widgets.setCurrentWidget(self.config_page)
+        self.highlight_button()
+
+    def highlight_button(self):
+        self.btn_home.setAutoFillBackground(False)
+        self.btn_graphs.setAutoFillBackground(False)
+        self.btn_connect.setAutoFillBackground(False)
+        self.btn_config.setAutoFillBackground(False)
+
+        if self.page_widgets.currentWidget().objectName() == 'home_page':
+            self.btn_home.setAutoFillBackground(True)
+        if self.page_widgets.currentWidget().objectName() == 'graph_page':
+            self.btn_graphs.setAutoFillBackground(True)
+        if self.page_widgets.currentWidget().objectName() == 'connection_page':
+            self.btn_connect.setAutoFillBackground(True)
+        if self.page_widgets.currentWidget().objectName() == 'config_page':
+            self.btn_config.setAutoFillBackground(True)
 
     def toggleMenu(self, maxWidth):
         width = self.slide_menu.width()
@@ -30,6 +64,8 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
         self.animation.setStartValue(width)
         self.animation.setEndValue(widthExtended)
         self.animation.start()
+
+        self.highlight_button()
 
 
 if __name__ == '__main__':
