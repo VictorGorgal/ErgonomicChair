18a3fcb0f76068b7eb00da98a7665b0cd0a2ba49
Victor Gorgal <victorgorgal@gmail.com>
2022-09-19 23:45:22 -0300

    feat: stuff

--- PCB/test1/test1.ino
+++ PCB/test1/test1.ino
@@ -11,6 +11,18 @@
 #define MQTT_PASSWD "morenamorenas"
 #define MQTT_CLIENT_ID "node_test_1"
 
+#define ANALOG_IN A0  // Analog input
+// Sensors
+#define CONTROL D8
+#define SEAT1 D7
+#define SEAT2 D6
+#define SEAT3 D5
+#define SEAT4 D0
+#define COASTER1 D4
+#define COASTER2 D3
+#define COASTER3 D2
+#define COASTER4 D1
+
 long delta;
 
 WiFiClient espClient;
@@ -61,26 +73,162 @@ void setup() {
   Serial.begin(115200);
   
   delta = millis();
+
+  pinMode(ANALOG_IN, INPUT);
+  pinMode(CONTROL, OUTPUT);
+  pinMode(SEAT1, OUTPUT);
+  pinMode(SEAT2, OUTPUT);
+  pinMode(SEAT3, OUTPUT);
+  pinMode(SEAT4, OUTPUT);
+  pinMode(COASTER1, OUTPUT);
+  pinMode(COASTER2, OUTPUT);
+  pinMode(COASTER3, OUTPUT);
+  pinMode(COASTER4, OUTPUT);
   
-  connectWifi();
+//  connectWifi();
   
-  setMqtt();
-  connectMqtt();
+//  setMqtt();
+//  connectMqtt();
 }
 
 void loop() {
-  client.loop();
+//  client.loop();
 
-  if (!client.connected()) {
-    connectMqtt();
-  }
+//  if (!client.connected()) {
+//    connectMqtt();
+//  }
 
-  if (millis() - delta > 60000) {
-    Serial.println("Sending...");
+//  if (millis() - delta > 60000) {
+//    Serial.println("Sending...");
     
     // Don't send if all sensors are 0
-    publish_MQTT("1;1;1;1;1;1;1;1");
+//    publish_MQTT("1;1;1;1;1;1;1;1");
 
-    delta = millis();
-  }
+  digitalWrite(CONTROL, HIGH);
+  digitalWrite(SEAT1, LOW);
+  digitalWrite(SEAT2, LOW);
+  digitalWrite(SEAT3, LOW);
+  digitalWrite(SEAT4, LOW);
+  digitalWrite(COASTER1, LOW);
+  digitalWrite(COASTER2, LOW);
+  digitalWrite(COASTER3, LOW);
+  digitalWrite(COASTER4, LOW);
+  delay(5);
+
+  Serial.print(analogRead(ANALOG_IN));
+
+  digitalWrite(CONTROL, LOW);
+  digitalWrite(SEAT1, HIGH);
+  digitalWrite(SEAT2, LOW);
+  digitalWrite(SEAT3, LOW);
+  digitalWrite(SEAT4, LOW);
+  digitalWrite(COASTER1, LOW);
+  digitalWrite(COASTER2, LOW);
+  digitalWrite(COASTER3, LOW);
+  digitalWrite(COASTER4, LOW);
+  delay(5);
+
+  Serial.print(", ");
+  Serial.print(analogRead(ANALOG_IN));
+
+  digitalWrite(CONTROL, LOW);
+  digitalWrite(SEAT1, LOW);
+  digitalWrite(SEAT2, HIGH);
+  digitalWrite(SEAT3, LOW);
+  digitalWrite(SEAT4, LOW);
+  digitalWrite(COASTER1, LOW);
+  digitalWrite(COASTER2, LOW);
+  digitalWrite(COASTER3, LOW);
+  digitalWrite(COASTER4, LOW);
+  delay(5);
+
+  Serial.print(", ");
+  Serial.print(analogRead(ANALOG_IN));
+
+  digitalWrite(CONTROL, LOW);
+  digitalWrite(SEAT1, LOW);
+  digitalWrite(SEAT2, LOW);
+  digitalWrite(SEAT3, HIGH);
+  digitalWrite(SEAT4, LOW);
+  digitalWrite(COASTER1, LOW);
+  digitalWrite(COASTER2, LOW);
+  digitalWrite(COASTER3, LOW);
+  digitalWrite(COASTER4, LOW);
+  delay(5);
+
+  Serial.print(", ");
+  Serial.print(analogRead(ANALOG_IN));
+
+  digitalWrite(CONTROL, LOW);
+  digitalWrite(SEAT1, LOW);
+  digitalWrite(SEAT2, LOW);
+  digitalWrite(SEAT3, LOW);
+  digitalWrite(SEAT4, HIGH);
+  digitalWrite(COASTER1, LOW);
+  digitalWrite(COASTER2, LOW);
+  digitalWrite(COASTER3, LOW);
+  digitalWrite(COASTER4, LOW);
+  delay(5);
+
+  Serial.print(", ");
+  Serial.print(analogRead(ANALOG_IN));
+
+  digitalWrite(CONTROL, LOW);
+  digitalWrite(SEAT1, LOW);
+  digitalWrite(SEAT2, LOW);
+  digitalWrite(SEAT3, LOW);
+  digitalWrite(SEAT4, LOW);
+  digitalWrite(COASTER1, HIGH);
+  digitalWrite(COASTER2, LOW);
+  digitalWrite(COASTER3, LOW);
+  digitalWrite(COASTER4, LOW);
+  delay(5);
+
+  Serial.print(", ");
+  Serial.print(analogRead(ANALOG_IN));
+
+  digitalWrite(CONTROL, LOW);
+  digitalWrite(SEAT1, LOW);
+  digitalWrite(SEAT2, LOW);
+  digitalWrite(SEAT3, LOW);
+  digitalWrite(SEAT4, LOW);
+  digitalWrite(COASTER1, LOW);
+  digitalWrite(COASTER2, HIGH);
+  digitalWrite(COASTER3, LOW);
+  digitalWrite(COASTER4, LOW);
+  delay(5);
+
+  Serial.print(", ");
+  Serial.print(analogRead(ANALOG_IN));
+
+  digitalWrite(CONTROL, LOW);
+  digitalWrite(SEAT1, LOW);
+  digitalWrite(SEAT2, LOW);
+  digitalWrite(SEAT3, LOW);
+  digitalWrite(SEAT4, LOW);
+  digitalWrite(COASTER1, LOW);
+  digitalWrite(COASTER2, LOW);
+  digitalWrite(COASTER3, HIGH);
+  digitalWrite(COASTER4, LOW);
+  delay(5);
+
+  Serial.print(", ");
+  Serial.print(analogRead(ANALOG_IN));
+
+  digitalWrite(CONTROL, LOW);
+  digitalWrite(SEAT1, LOW);
+  digitalWrite(SEAT2, LOW);
+  digitalWrite(SEAT3, LOW);
+  digitalWrite(SEAT4, LOW);
+  digitalWrite(COASTER1, LOW);
+  digitalWrite(COASTER2, LOW);
+  digitalWrite(COASTER3, LOW);
+  digitalWrite(COASTER4, HIGH);
+  delay(5);
+
+  Serial.print(", ");
+  Serial.println(analogRead(ANALOG_IN));
+
+//    delta = millis();
+//  }
 }
